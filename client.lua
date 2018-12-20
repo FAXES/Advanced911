c932fa23c8e15b3fc400f87939edc0346ea5216f
Lenzh <lorenzo0109@hotmail.nl>
2018-12-20 19:28:08 +0100

    Added Location support aswell

--- client.lua
+++ client.lua
@@ -19,19 +19,24 @@ end)
 ---------------------------------------------------------------------------------
 RegisterNetEvent('Fax:SendCall')
 AddEventHandler('Fax:SendCall', function(service, desc, callid)
+    local src = s
     local ped = GetPlayerPed(PlayerId())
+    local coords = GetEntityCoords(GetPlayerPed(GetPlayerFromServerId(src)))
+    local street1 = GetStreetNameAtCoord(coords.x, coords.y, coords.z, Citizen.ResultAsInteger(), Citizen.ResultAsInteger())
+    local streetName = (GetStreetNameFromHashKey(street1))
+
 
 	if service == "pd" then
 		if PlayerData.job ~= nil and PlayerData.job.name == 'police' then
-			TriggerServerEvent("Fax:SendCallToTeam", service, desc, callid)
+			TriggerServerEvent("Fax:SendCallToTeam", service, desc, callid, streetName)
 		end
 	 elseif service == "ems" then
 		if PlayerData.job ~= nil and PlayerData.job.name == 'ambulance' then
-			TriggerServerEvent("Fax:SendCallToTeam", service, desc, callid)
+			TriggerServerEvent("Fax:SendCallToTeam", service, desc, callid, streetName)
         end
     elseif service == "all" then
 		if PlayerData.job ~= nil and PlayerData.job.name == 'ambulance' or PlayerData.job ~= nil and PlayerData.job.name == 'police' then
-			TriggerServerEvent("Fax:SendCallToTeam", service, desc, callid)
+			TriggerServerEvent("Fax:SendCallToTeam", service, desc, callid, streetName)
         end
     end
 end)
